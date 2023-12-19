24d7d9cb4c99790180a1b738d69152038e139873
Beau Cameron <beau@Beaucameron.net>
2023-12-19 10:04:46 -0500

    Update admin.ts

--- test/graph/admin.ts
+++ test/graph/admin.ts
@@ -1,12 +1,14 @@
-import { getRandomString, stringIsNullOrEmpty } from "@pnp/core";
 import { expect } from "chai";
+import { getRandomString, stringIsNullOrEmpty } from "@pnp/core";
+import { pnpTest } from "../pnp-test.js";
 import "@pnp/graph/admin";
 
 describe("Admin", function () {
     const customUserProperty = "CustomAttribute1";
     let propertyId = "";
+
     // Ensure we have the data to test against
-    before(async function () {
+    before(async function  () {
 
         if (!this.pnp.settings.enableWebTests || stringIsNullOrEmpty(this.pnp.settings.testUser)) {
             this.skip();
@@ -32,88 +34,93 @@ describe("Admin", function () {
         }
     });
 
-    it("People - Get SharePoint Settings", async function () {
-        const sharePointSettings = await this.pnp.graph.admin.sharepoint.settings();
-        return expect(sharePointSettings.availableManagedPathsForSiteCreation.length > 0).is.true;
+    describe("SharePoint", function () {
+        it("Get SharePoint Settings", pnpTest("923c1bd6-8621-41d2-9ea9-004a4a735c9f", async function  () {
+            const sharePointSettings = await this.pnp.graph.admin.sharepoint.settings();
+            return expect(sharePointSettings.availableManagedPathsForSiteCreation.length > 0).is.true;
+        }));
+
+        it("Update SharePoint Settings", pnpTest("bbf52535-3a7e-452b-b0eb-9940832163aa", async function  () {
+            const sharePointSettings = await this.pnp.graph.admin.sharepoint.settings.update({ deletedUserPersonalSiteRetentionPeriodInDays: 30 });
+            return expect(sharePointSettings.deletedUserPersonalSiteRetentionPeriodInDays === 30).is.true;
+        }));
     });
 
-    it("People - Update SharePoint Settings", async function () {
-        const sharePointSettings = await this.pnp.graph.admin.sharepoint.settings.update({ deletedUserPersonalSiteRetentionPeriodInDays: 30 });
-        return expect(sharePointSettings.deletedUserPersonalSiteRetentionPeriodInDays === 30).is.true;
+    describe("People", function () {
+        it("Get People Settings", pnpTest("9bd5a022-65d3-4a34-b8c4-c74381b98551", async function  () {
+            const settings = await this.pnp.graph.admin.people();
+            return expect(settings.profileCardProperties).to.be.an("array") && expect(settings).to.haveOwnProperty("id");
+        }));
+
+        it("Get Pronoun Settings", pnpTest("bbc0e5af-3620-4164-9120-556ac534db39", async function  () {
+            const settings = await this.pnp.graph.admin.people.pronounSettings();
+            return expect(settings.isEnabledInOrganization).to.be.an("boolean");
+        }));
+
+        it.skip("Update Pronoun Settings", pnpTest("830c2b41-5642-40d6-8585-3e26207e3f13", async function  () {
+            const settings = await this.pnp.graph.admin.people.pronounSettings.update({
+                isEnabledInOrganization: true,
+            });
+            return expect(settings.isEnabledInOrganization).is.true;
+        }));
+
+        it.skip("Add Profile Card Property", pnpTest("49b98899-0af3-4b8b-8f66-3748410420b7", async function  () {
+            const property = await this.pnp.graph.admin.people.profileCardProperties.add({
+                directoryPropertyName: "CustomAttribute2",
+                annotations: [{
+                    displayName: "Cost Center",
+                    localizations: [
+                        {
+                            languageTag: "ru-RU",
+                            displayName: "центр затрат",
+                        },
+                    ],
+                }],
+            });
+            return expect(property.id).is.not.null;
+        }));
+
+        it("Get Profile Card Property", pnpTest("05d8f50a-1b47-4631-9576-2aa3c5efcf75", async function  () {
+            const property = await this.pnp.graph.admin.people.profileCardProperties.getById(customUserProperty)();
+            return expect(property.id).is.not.null;
+        }));
+
+        it("Update Profile Card Property", pnpTest("04fb914e-41c6-4b8e-a326-63c41e6672a4", async function  () {
+            const displayName = getRandomString(5) + "Cost Center";
+            const property = await this.pnp.graph.admin.people.profileCardProperties.getById(customUserProperty).update({
+                directoryPropertyName: this.customUserProperty,
+                annotations: [{
+                    displayName: getRandomString(5) + "Cost Center",
+                    localizations: [
+                        {
+                            languageTag: "ru-RU",
+                            displayName: "центр затрат",
+                        },
+                    ],
+                }],
+            });
+            return expect(property.annotations[0]?.displayName).equals(displayName);
+        }));
+
+        it("Delete Profile Card Property", pnpTest("fbfae956-d776-4bd7-8ad2-3db384ec02c3", async function  () {
+            const property = await this.pnp.graph.admin.people.profileCardProperties.add({
+                directoryPropertyName: getRandomString(5) + "CustomAttribute2",
+                annotations: [{
+                    displayName: "Cost Center",
+                    localizations: [
+                        {
+                            languageTag: "ru-RU",
+                            displayName: "центр затрат",
+                        },
+                    ],
+                }],
+            });
+            const response = await this.pnp.graph.admin.people.profileCardProperties.getById(property.id).delete();
+            return expect(response).is.ok;
+        }));
     });
 
-    it("People - Get People Settings", async function () {
-        const settings = await this.pnp.graph.admin.people();
-        return expect(settings.profileCardProperties).to.be.an("array") && expect(settings).to.haveOwnProperty("id");
-    });
-
-    it("People - Get Pronoun Settings", async function () {
-        const settings = await this.pnp.graph.admin.people.pronounSettings();
-        return expect(settings.isEnabledInOrganization).to.be.an("boolean");
-    });
-
-    it.skip("People - Update Pronoun Settings", async function () {
-        const settings = await this.pnp.graph.admin.people.pronounSettings.update({
-            isEnabledInOrganization: true,
-        });
-        return expect(settings.isEnabledInOrganization).is.true;
-    });
-
-    it.skip("ProfileCard - Add Profile Card Property", async function () {
-        const property = await this.pnp.graph.admin.people.profileCardProperties.add({
-            directoryPropertyName: "CustomAttribute2",
-            annotations: [{
-                displayName: "Cost Center",
-                localizations: [
-                    {
-                        languageTag: "ru-RU",
-                        displayName: "центр затрат",
-                    },
-                ],
-            }],
-        });
-        return expect(property.id).is.not.null;
-    });
-
-    it("ProfileCard - Get Profile Card Property", async function () {
-        const property = await this.pnp.graph.admin.people.profileCardProperties.getById(customUserProperty)();
-        return expect(property.id).is.not.null;
-    });
-
-    it("ProfileCard - Update Profile Card Property", async function () {
-        const displayName = getRandomString(5) + "Cost Center";
-        const property = await this.pnp.graph.admin.people.profileCardProperties.getById(customUserProperty).update({
-            directoryPropertyName: this.customUserProperty,
-            annotations: [{
-                displayName: getRandomString(5) + "Cost Center",
-                localizations: [
-                    {
-                        languageTag: "ru-RU",
-                        displayName: "центр затрат",
-                    },
-                ],
-            }],
-        });
-        return expect(property.annotations[0]?.displayName).equals(displayName);
-    });
-    it("ProfileCard - Delete Profile Card Property", async function () {
-        const property = await this.pnp.graph.admin.people.profileCardProperties.add({
-            directoryPropertyName: getRandomString(5) + "CustomAttribute2",
-            annotations: [{
-                displayName: "Cost Center",
-                localizations: [
-                    {
-                        languageTag: "ru-RU",
-                        displayName: "центр затрат",
-                    },
-                ],
-            }],
-        });
-        const response = await this.pnp.graph.admin.people.profileCardProperties.getById(property.id).delete();
-        return expect(response).is.ok;
-    });
-
-    after(async function () {
+    after(async function  () {
 
         if (!stringIsNullOrEmpty(propertyId)) {
             try {
